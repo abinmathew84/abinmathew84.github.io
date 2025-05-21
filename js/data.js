77129d4e5d42421c56aaea925b576ea7b1627156
Abin Mathew <abinmathew84@gmail.com>
2025-05-21 18:05:49 +0100

    feat: update skills data; add new technologies and improve descriptions for clarity

--- js/data.js
+++ js/data.js
@@ -39,7 +39,7 @@ window.skillsData = {
             icon: `<svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 mr-3 text-black dark:text-white" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 12h14M5 12a2 2 0 01-2-2V6a2 2 0 012-2h14a2 2 0 012 2v4a2 2 0 01-2 2M5 12a2 2 0 00-2 2v4a2 2 0 002 2h14a2 2 0 002-2v-4a2 2 0 00-2-2m-2-4h.01M17 16h.01" />
                 </svg>`,
-            skills: ["Function Apps, Web Apps", "Storage Account, Cosmos DB, Blob Storage", "Service Bus, Key Vault, Azure AD", "Virtual Machine, App Service, Resource Groups"]
+            skills: ["Function Apps, Web Apps, Logic Apps", "Storage Account, CosmosDB, Blob", "Service Bus, Key Vault, Azure AD", "Azure VM, App Service, Resource Groups"]
         },
         {
             id: "databases",
@@ -47,7 +47,7 @@ window.skillsData = {
             icon: `<svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 mr-3 text-black dark:text-white" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 7v10c0 2.21 3.582 4 8 4s8-1.79 8-4V7M4 7c0 2.21 3.582 4 8 4s8-1.79 8-4M4 7c0-2.21 3.582-4 8-4s8 1.79 8 4m0 5c0 2.21-3.582 4-8 4s-8-1.79-8-4" />
                 </svg>`,
-            skills: ["SQL Server, MySQL, Access DB", "IBM DB2, VSAM"]
+            skills: ["SQL Server", "MySQL", "Access DB", "IBM DB2, VSAM"]
         },
         {
             id: "ai-ml",
@@ -55,7 +55,7 @@ window.skillsData = {
             icon: `<svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 mr-3 text-black dark:text-white" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9.663 17h4.673M12 3v1m6.364 1.636l-.707.707M21 12h-1M4 12H3m3.343-5.657l-.707-.707m2.828 9.9a5 5 0 117.072 0l-.548.547A3.374 3.374 0 0014 18.469V19a2 2 0 11-4 0v-.531c0-.895-.356-1.754-.988-2.386l-.548-.547z" />
                 </svg>`,
-            skills: ["OpenAI (GPT), Anthropic (Claude)", "Google (Gemini), Ollama (Local)", "RAG, Vector Databases, MCP"]
+            skills: ["OpenAI (GPT), Anthropic (Claude)", "Google (Gemini), Ollama (Local)", "RAG, Vector Databases, MCP", "Generative AI, Agentic AI"]
         },
         {
             id: "scripting-tools",
